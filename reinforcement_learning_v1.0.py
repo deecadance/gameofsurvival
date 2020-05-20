fec2a9139f2a06e0fdaca1030735c117542788b3
Simone Di Cataldo <simonedicataldo@MacBook-Pro-di-Simone.local>
2020-05-20 10:40:15 +0200

    update, softmax

--- reinforcement_learning_v1.0.py
+++ reinforcement_learning_v1.0.py
@@ -4,11 +4,13 @@
 ######################
 ####  PARAMETERS. ####
 ######################
-load_model = True       ### Load model or create from scratch?
+do_load_model = False       ### Load model or create from scratch?
+model_name = 'player_v3.h5'
+fixed_start = True      ### Always start from same config?
 run = 1			 ### Index for saving figures
-epochs = 100             ### Number of cycles
+epochs = 200             ### Number of cycles
 max_steps = 1000	 ### Max steps per epoch
-world_size = 200         ### Linear dimensions of the (squared) world
+world_size = 20         ### Linear dimensions of the (squared) world
 see_size = 20            ### DO NOT CHANGE. Dimension of the observable world.
                          ### If you change it: you have to change the DRL model input size
 group_size = 6           ### DO NOT CHANGE. Linear dimensions of the (suqared) community
@@ -21,8 +23,10 @@ p_fuzzy = 1.0/world_size/world_size     ### Note that approx. P(1 switch) = worl
 
 alive = 1              ### Def. 1 alive and 0 dead 
 dead = 0               ###
+seed = 132	       ### Fixed start trial 
 
 from IPython import display
+import gc
 import numpy as np
 import matplotlib as mtplt
 import matplotlib.pyplot as plt
@@ -34,7 +38,7 @@ import copy
 from matplotlib import rc
 
 import random
-start = time.time()
+random.seed(seed)
 
 from gameofsurvival_libraries import init_grid, grid_to_list, grid_to_set, list_to_grid, set_to_grid
 from gameofsurvival_libraries import get_neighbours, apply_rules, fuzzy_rules, calculate_action, time_step
@@ -43,6 +47,8 @@ from gameofsurvival_libraries import alive_cells, counter
 
 ### Generate world and group at random
 world = init_grid(world_size, filling)
+if fixed_start:
+   fixed_world = world
 group = init_grid(group_size, filling)
 
 ### PLOT INITIAL STATE
@@ -57,17 +63,18 @@ print("###STARTING WORLD CONFIGURATION###")
 import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 print(sys.path)
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, ZeroPadding2D, Flatten
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, ZeroPadding2D, Flatten, Dense
 from tensorflow.keras.models import Model, Sequential, load_model
 from tensorflow.keras.callbacks import TensorBoard
 from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
+import keras.backend as K_backend
 
-if load_model:
+if do_load_model:
     print('Loading model :')
     t0 = time.time()
-    player = load_model('player_v2.h5')
+    player = load_model(model_name)
     t1 = time.time()
     print('Model loaded in: ', t1-t0)
 else:
@@ -78,16 +85,19 @@ else:
    ## 20x20x16
    player.add(MaxPooling2D((2, 2), padding='same'))
    ## 10x10x16
-   player.add(Conv2D(36, (3, 3), activation='relu', padding='same'))
-   ## 10x10x36
+   player.add(Conv2D(24, (3, 3), activation='relu', padding='same'))
+   ## 10x10x24
    player.add(MaxPooling2D((2, 2), padding='same'))
-   ## 5x5x36
+   ## 5x5x24
    player.add(Conv2D(36, (2, 2), activation='relu', padding='same'))
    ## 5x5x36
-   player.add(MaxPooling2D((6, 6), padding='same', name='encoder'))
+   player.add(MaxPooling2D((5, 5), padding='same', name='encoder'))
    ## 1x1x36
    player.add(Flatten())
-   player.compile(loss='mse', optimizer='adam', metrics=['mae'])
+   player.add(Dense(36, activation = 'relu'))  ## Dense layer with 36 nodes
+   player.add(Dense(36, activation = 'softmax')) ## Final layer with 36 output nodes and softmax
+    ## Softmax ensures that the outputs are probabilities of target labels
+   player.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['mae'])
 
 
 y = 0.95
@@ -96,6 +106,10 @@ decay_factor = 0.999
 
 max_t_list = []
 r_avg_list = []
+grp_avg_list = []
+ctrl_avg_list = []
+wrld_avg_list = []
+
 sight_index_1 = int((world_size-see_size)/2)
 sight_index_2 = int((world_size+see_size)/2)
 group_index_1 = int((world_size-group_size)/2)
@@ -104,12 +118,21 @@ group_index_2 = int((world_size+group_size)/2)
 action = np.zeros(group_size*group_size)
 
 for i in range(epochs):
-    world = init_grid(world_size, filling)     ### Initialize world
+    if (i%21 == 20):
+        player = load_model(model_name)
+
+    if fixed_start:
+        world = fixed_world
+    else:
+        world = init_grid(world_size, filling)     ### Initialize world
     alive_cells = grid_to_set(world)
     eps *= decay_factor               ### Decay probability of random action
     print("Epoch", i+1,  "of ", epochs)
     done = False
     r_sum = 0
+    grp_sum = 0
+    ctrl_sum = 0
+    wrld_sum = 0
     done = 0
     time = []
     group_alive_percent = []
@@ -153,6 +176,9 @@ for i in range(epochs):
         world_alive_percent.append((world_alive_cells-group_alive_cells-control_alive_cells)/(world_size*world_size-2*group_size*group_size)*100)
         reward_history.append(reward)
         time.append(j)
+        grp_sum += group_alive_cells
+        ctrl_sum += control_alive_cells
+        wrld_sum += world_alive_cells
         ### INFORMATIVE MESSAGE
         if (j%(int(max_steps/4))==0):
             print(float(j)/max_steps*100, "% of steps done")
@@ -180,6 +206,9 @@ for i in range(epochs):
             break
     max_t_list.append(j)
     r_avg_list.append(r_sum/j)
+    grp_avg_list.append(grp_sum/j)
+    ctrl_avg_list.append(ctrl_sum/j)
+    wrld_avg_list.append(wrld_sum/j)
     name = 'epoch_' + str(i) + '_days_' + str(j) + '_metrics.txt'
     time_nparray = np.asarray(time)
     wap_nparray = np.asarray(world_alive_percent)
@@ -188,10 +217,19 @@ for i in range(epochs):
     rew_nparray = np.asarray(reward_history)
     np.savetxt(name,np.column_stack([time_nparray,wap_nparray,gap_nparray,cap_nparray, rew_nparray]),
 header='step, world, group,control, reward')
+    if (i%21==19):
+        print("Intermediate save.. flushing RAM")
+        player.save(model_name)
+        K_backend.clear_session()
+        gc.collect()
 
 max_t_nparray = np.asarray(max_t_list)
 rew_avg_nparray = np.asarray(r_avg_list)
+grp_avg_nparray = np.asarray(grp_avg_list)
+ctrl_avg_nparray = np.asarray(ctrl_avg_list)
+wrld_avg_nparray = np.asarray(wrld_avg_list)
 np.savetxt('game_length.txt',np.transpose(max_t_nparray))
-np.savetxt('reward_avg.txt',np.transpose(rew_avg_nparray))
+np.savetxt('data_avg.txt',np.transpose([rew_avg_nparray, max_t_nparray, grp_avg_nparray, ctrl_avg_nparray, wrld_avg_nparray]), header='avg_reward, game_length, avg_group_size, avg_ctrl_group_size')
 
-player.save('player_v2.h5')
+player.save(model_name)
+exit()
